7ce2ace61a49ae48f9a8393205f446ad2b2e0cff
Olivér Nagy <nagy.oliver@uni-neumann.hu>
2023-10-04 16:16:27 +0200

    [MOD] organized Normal distribution's test cases into TestClass

--- tests/utils/test_distributions.py
+++ tests/utils/test_distributions.py
@@ -117,126 +117,110 @@ class TestUniformDistribution:
         assert result == approx(expected)
 
 
-def test_normal_distribution_input():
-    # Arrange
+class TestNormalDistribution:
     expected_value = 1
     variance = 4
     rand_gen = random
 
-    # Act
-    dist = dst.NormalDistribution(rand_gen, expected_value, variance)
-    result1 = dist.loc
-    result2 = dist.scale
-    result3 = dist.rand
+    def test_input(self):
+        # Arrange
 
-    # Assert
-    assert result1 == expected_value
-    assert result2 == variance
-    assert result3 == rand_gen
+        # Act
+        dist = dst.NormalDistribution(self.rand_gen, self.expected_value, self.variance)
+        result1 = dist.loc
+        result2 = dist.scale
+        result3 = dist.rand
 
+        # Assert
+        assert result1 == self.expected_value
+        assert result2 == self.variance
+        assert result3 == self.rand_gen
 
-def test_normal_distribution_pdf():
-    # Arrange
-    expected_value = 1
-    variance = 4
-    rand_gen = random
-    test_value = 0.52
-    expected = 0.19380830756250708
+    def test_pdf(self):
+        # Arrange
+        test_value = 0.52
+        expected = 0.19380830756250708
 
-    # Act
-    dist = dst.NormalDistribution(rand_gen, expected_value, variance)
-    result = dist.pdf(test_value)
+        # Act
+        dist = dst.NormalDistribution(self.rand_gen, self.expected_value, self.variance)
+        result = dist.pdf(test_value)
 
-    # Assert
-    assert result == approx(expected)
+        # Assert
+        assert result == approx(expected)
 
+    def test_cdf(self):
+        # Arrange
+        test_value = 0.52
+        expected = 0.4051651283022042
 
-def test_normal_distribution_cdf():
-    # Arrange
-    expected_value = 1
-    variance = 4
-    rand_gen = random
-    test_value = 0.52
-    expected = 0.4051651283022042
+        # Act
+        dist = dst.NormalDistribution(self.rand_gen, self.expected_value, self.variance)
+        result = dist.cdf(test_value)
 
-    # Act
-    dist = dst.NormalDistribution(rand_gen, expected_value, variance)
-    result = dist.cdf(test_value)
+        # Assert
+        assert result == approx(expected)
 
-    # Assert
-    assert result == approx(expected)
+    def test_ppf(self):
+        # Arrange
+        test_value = 0.52
+        expected = 1.1003071669294673
 
+        # Act
+        dist = dst.NormalDistribution(self.rand_gen, self.expected_value, self.variance)
+        result = dist.ppf(test_value)
 
-def test_normal_distribution_ppf():
-    # Arrange
-    expected_value = 1
-    variance = 4
-    rand_gen = random
-    test_value = 0.52
-    expected = 1.1003071669294673
+        # Assert
+        assert result == approx(expected)
 
-    # Act
-    dist = dst.NormalDistribution(rand_gen, expected_value, variance)
-    result = dist.ppf(test_value)
+    def test_gen_random(self):
+        # Arrange
+        expected_value = 2
+        variance = 1
+        expected = [2.3569270708684202, 0.040220005035863426, 1.402327737513401, 1.2386055079365632, 2.6325040365145673,
+                    2.4584891926826047, 3.2382028242997416, 0.640150850153381,
+                    1.8030205757199995, 0.11621777518302956, 1.2231985982110944, 2.01342411975449, 0.06566489228213013,
+                    1.1542196715540782, 2.3850084912801304, 2.1128909153430118,
+                    1.2292940494954125, 2.2256564500200304, 2.8757994177707253, -0.48383725525960486, 2.862592607409397,
+                    2.5190566888467996, 1.5882204790694017, 0.9867881879196907,
+                    3.7192225537107695, 1.5782247741276278, 0.6759658001856772, 0.6995090430631992, 3.025746123557081,
+                    2.263003427582639, 2.867362442337795, 2.6120020116542166,
+                    2.0909355314968727, 3.9286971357294815, 1.6906676885242782, 2.1308186901524557, 2.9518152797052206,
+                    2.301595425973794, 3.088020178981573, 2.1951242106524,
+                    2.5375955145460214, 0.3132361404850703, 1.2542137565594773, 1.4448264039009358, 0.5935277994004471,
+                    1.2703133982786134, 0.7241339067558314, 1.4111280956859877,
+                    2.3469470329438633, 1.6544279550689527, 1.668625910909558, 1.1918670680910513, 1.3780209504710874,
+                    3.5272823172999788, 2.3800218045840524, 2.277054851322021,
+                    1.0503247637097153, 2.6101742217458517, 1.019430310336593, 1.6930886944354917, 4.308824917643308,
+                    2.358458151351882, 2.143240161622287, 2.4806412426035793,
+                    3.0062482660344427, 2.7587532488261512, 1.2580145380087953, 0.1492149857044469, 1.5195480979271214,
+                    1.3803401462847624, 1.1969843467488075, 3.579678240030068,
+                    3.157018634645539, 1.5173661806042777, 2.400045954961469, 1.7353301454403307, 3.3693022322679735,
+                    1.8966733247997656, 1.3716280883261456, 1.3148591032893364,
+                    2.1544388392859126, 1.3650840241901614, 2.213639723576354, 3.2692434941851385, 1.7451008726448132,
+                    1.225510742268578, 4.81191268452039, 2.023881146827212,
+                    0.6648242275156562, 0.3265195439893258, 0.7716037654062655, 2.3250966601676577, 2.8136573541287966,
+                    1.8036291796594455, 0.4741828657634508, 1.6987692893071888,
+                    4.662457368477558, 2.073043741459248, 3.896884036534805, 3.083829065345797, -0.27406557889967464,
+                    2.5849871438128904, 2.4724869393799453, 2.0928039169305044]
 
-    # Assert
-    assert result == approx(expected)
+        # Act
+        dist = dst.NormalDistribution(self.rand_gen, expected_value, variance)
+        dist.rand.seed(42)
+        result = [dist.gen_rand() for _ in range(104)]
 
+        # Assert
+        assert result == approx(expected)
 
-def test_normal_distribution_gen_random():
-    # Arrange
-    expected_value = 2
-    variance = 1
-    rand_gen = random
-    expected = [2.3569270708684202, 0.040220005035863426, 1.402327737513401, 1.2386055079365632, 2.6325040365145673,
-                2.4584891926826047, 3.2382028242997416, 0.640150850153381,
-                1.8030205757199995, 0.11621777518302956, 1.2231985982110944, 2.01342411975449, 0.06566489228213013,
-                1.1542196715540782, 2.3850084912801304, 2.1128909153430118,
-                1.2292940494954125, 2.2256564500200304, 2.8757994177707253, -0.48383725525960486, 2.862592607409397,
-                2.5190566888467996, 1.5882204790694017, 0.9867881879196907,
-                3.7192225537107695, 1.5782247741276278, 0.6759658001856772, 0.6995090430631992, 3.025746123557081,
-                2.263003427582639, 2.867362442337795, 2.6120020116542166,
-                2.0909355314968727, 3.9286971357294815, 1.6906676885242782, 2.1308186901524557, 2.9518152797052206,
-                2.301595425973794, 3.088020178981573, 2.1951242106524,
-                2.5375955145460214, 0.3132361404850703, 1.2542137565594773, 1.4448264039009358, 0.5935277994004471,
-                1.2703133982786134, 0.7241339067558314, 1.4111280956859877,
-                2.3469470329438633, 1.6544279550689527, 1.668625910909558, 1.1918670680910513, 1.3780209504710874,
-                3.5272823172999788, 2.3800218045840524, 2.277054851322021,
-                1.0503247637097153, 2.6101742217458517, 1.019430310336593, 1.6930886944354917, 4.308824917643308,
-                2.358458151351882, 2.143240161622287, 2.4806412426035793,
-                3.0062482660344427, 2.7587532488261512, 1.2580145380087953, 0.1492149857044469, 1.5195480979271214,
-                1.3803401462847624, 1.1969843467488075, 3.579678240030068,
-                3.157018634645539, 1.5173661806042777, 2.400045954961469, 1.7353301454403307, 3.3693022322679735,
-                1.8966733247997656, 1.3716280883261456, 1.3148591032893364,
-                2.1544388392859126, 1.3650840241901614, 2.213639723576354, 3.2692434941851385, 1.7451008726448132,
-                1.225510742268578, 4.81191268452039, 2.023881146827212,
-                0.6648242275156562, 0.3265195439893258, 0.7716037654062655, 2.3250966601676577, 2.8136573541287966,
-                1.8036291796594455, 0.4741828657634508, 1.6987692893071888,
-                4.662457368477558, 2.073043741459248, 3.896884036534805, 3.083829065345797, -0.27406557889967464,
-                2.5849871438128904, 2.4724869393799453, 2.0928039169305044]
+    def test_moments(self):
+        # Arrange
+        expected = [self.expected_value, self.variance, 0, 0]
 
-    # Act
-    dist = dst.NormalDistribution(rand_gen, expected_value, variance)
-    dist.rand.seed(42)
-    result = [dist.gen_rand() for _ in range(104)]
+        # Act
+        dist = dst.NormalDistribution(self.rand_gen, self.expected_value, self.variance)
+        result = dist.mvsk()
 
-    # Assert
-    assert result == approx(expected)
-
-
-def test_normal_distribution_moments():
-    # Arrange
-    expected_value = 1
-    variance = 4
-    rand_gen = random
-    expected = [expected_value, variance, 0, 0]
-
-    # Act
-    dist = dst.NormalDistribution(rand_gen, expected_value, variance)
-    result = dist.mvsk()
-
-    # Assert
-    assert result == approx(expected)
+        # Assert
+        assert result == approx(expected)
 
 
 def test_cauchy_distribution_input():
