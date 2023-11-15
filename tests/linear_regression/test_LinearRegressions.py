2123407c5d4e72f11e3423329794ff94b3d427cc
nagy-oliver-neumann <nagy.oliver@uni-neumann.hu>
2023-11-15 18:58:19 +0100

    [ADD] 8th test's notebook and unit test

--- tests/linear_regression/test_LinearRegressions.py
+++ tests/linear_regression/test_LinearRegressions.py
@@ -6,6 +6,7 @@ from pytest import approx
 from src.linear_regression.LinearRegressions import (
     LinearRegressionSM,
     LinearRegressionNP,
+    LinearRegressionGLS,
 )
 
 
@@ -127,6 +128,70 @@ class TestLinearRegressionNP:
     def test_get_model_goodness_values(self):
         # Arrange
         expected = "Centered R-squared: 0.030, Adjusted R-squared: 0.015"
+
+        # Act
+        result = self.model.get_model_goodness_values()
+
+        # Assert
+        assert result == approx(expected)
+
+
+class TestLinearRegressionGLS:
+    ref_data = pd.read_parquet(
+        Path.cwd()
+        .parent.parent.joinpath("data")
+        .joinpath("weekly6")
+        .joinpath("toclean.parquet")
+    )
+    model = LinearRegressionGLS(
+        ref_data["ex_ret_1"], ref_data[["Mkt-RF", "SMB", "HML"]]
+    )
+    model.fit()
+
+    def test_get_params(self):
+        # Arrange
+        expected = [
+            0.016013386040414725,
+            -0.472579880012349,
+            0.4628817079467341,
+            -0.1571557759329604,
+        ]
+
+        # Act
+        result = self.model.get_params().tolist()
+
+        # Assert
+        assert result == approx(expected)
+
+    def test_pvalues(self):
+        # Arrange
+        expected = [
+            1.2783640812585872e-10,
+            5.920920032529536e-17,
+            1.9665957751158203e-09,
+            0.023852711998156494,
+        ]
+
+        # Act
+        result = self.model.get_pvalues().tolist()
+
+        # Assert
+        assert result == approx(expected)
+
+    def test_get_wald_test_result(self):
+        # Arrange
+        restriction_matrix = [[0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+        expected = "Wald: 39.252, p-value: 0.000"
+
+        # Act
+        result = self.model.get_wald_test_result(restriction_matrix)
+
+        # Assert
+        assert result == approx(expected)
+
+    def test_get_model_goodness_values(self):
+        # Arrange
+        expected = "Centered R-squared: 0.955, Adjusted R-squared: 0.954"
 
         # Act
         result = self.model.get_model_goodness_values()
